20ec7ddc2a2f062092b867b72c1ab5eb9008a7ee
edifff <mishapronin2015@gmail.com>
2026-04-15 01:33:49 +0300

    Добавлена базовая защита приложения

---
+++ identity-service/src/main/java/ru/rsreu/projectmanagment/identityservice/identityservice/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package ru.rsreu.projectmanagment.identityservice.identityservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.authorization.SingleResultAuthorizationManager.permitAll;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) {
+        httpSecurity.csrf(csfr-> csfr.disable());
+        httpSecurity.sessionManagement(session-> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth", "/register" ).permitAll()
+                .anyRequest().authenticated());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config){
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
