cfa86fb01d696d7428306af7a99559085d920c22
edifff <mishapronin2015@gmail.com>
2026-04-27 15:30:51 +0300

    начало разработки сервиса вакансий

--- identity-service/src/main/java/ru/rsreu/projectmanagment/identityservice/identityservice/data/repository/UserRepository.java
+++ identity-service/src/main/java/ru/rsreu/projectmanagment/identityservice/identityservice/data/repository/UserRepository.java
@@ -1,13 +1,14 @@
 package ru.rsreu.projectmanagment.identityservice.identityservice.data.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import ru.rsreu.projectmanagment.identityservice.identityservice.data.entity.User;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, UUID>{
     Optional<User> findById(UUID id);
 
     @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.email = :email")
