a4c0d67689dfc478fe5f64a9b14077107a604388
edifff <mishapronin2015@gmail.com>
2026-05-10 13:18:00 +0300

    Добавлена обработка PUTCH методов с локалхоста фронта

--- identity-service/src/main/java/ru/rsreu/projectmanagment/identityservice/identityservice/config/SecurityConfig.java
+++ identity-service/src/main/java/ru/rsreu/projectmanagment/identityservice/identityservice/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
                 "http://localhost:3000",
                 "http://localhost:4200"
         ));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
